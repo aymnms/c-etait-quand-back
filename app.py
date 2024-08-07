1653899329047c51346dbf56c24a8fe4b9c7dc88
Aymerick Michelet <micheletaymerick@gmail.com>
2024-08-07 19:48:50 +0200

    feat: add database

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import random
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cetaitquand.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pseudo = db.Column(db.String(80), nullable=False)
+    score = db.Column(db.Integer, default=0)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    answers = db.relationship('Answer', backref='player', lazy=True)
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    answer = db.Column(db.String(200), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'), nullable=False)
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(36), unique=True, nullable=False, default=str(uuid.uuid4()))
+    players = db.relationship('Player', backref='room', lazy=True)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(200), nullable=False)
+    solution = db.Column(db.String(200), nullable=False)
+    answers = db.relationship('Answer', backref='question', lazy=True)
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(200), nullable=False)
+    players = db.relationship('Player', backref='character', lazy=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
